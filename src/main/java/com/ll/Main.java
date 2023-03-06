12c71a2757aa7ca329cb3e8ab728f0adba12e3fc
깃wooqqq <sge0225@naver.com>
2023-03-06 15:03:21 +0900

    종료기능 구현, 올바르지 않은 명령어 처리

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,10 @@
-package org.example;
+package com.ll;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new App(new Scanner(System.in)).run();
     }
 }
