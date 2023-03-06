b511c5b0c6c98cbfad38e0084fea4e9543c84511
깃wooqqq <sge0225@naver.com>
2023-03-06 15:10:33 +0900

    AppTestRunner 도입해서, 중복제거

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTests {
+    // 테스트유틸 테스트 시작
     @Test
     @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
     public void t1() {
@@ -22,7 +23,7 @@ public class AppTests {
     @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
     public void t2() {
         // System.out 에 대한 화면출력 금지 시작
-        ByteArrayOutputStream output =  TestUtil.setOutToByteArray();
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
         System.out.print("안녕");
 
@@ -34,20 +35,13 @@ public class AppTests {
 
         assertThat(rs).isEqualTo("안녕");
     }
-
     // 테스트유틸 테스트 끝
 
     // 앱 테스트 시작
     @Test
     @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
     public void t3() {
-        Scanner sc = TestUtil.genScanner("종료");
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("");
 
         assertThat(rs)
                 .contains("== 명언 앱 ==")
@@ -59,16 +53,7 @@ public class AppTests {
     @Test
     @DisplayName("잘못된 명령어 입력에 대한 처리")
     public void t4() {
-        Scanner sc = TestUtil.genScanner("""
-                종료2
-                종료
-                """.stripIndent().trim());
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("종료2");
 
         assertThat(rs)
                 .contains("올바르지 않은 명령입니다.");
