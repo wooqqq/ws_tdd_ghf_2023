2cdc59314dc58ee02837e7940f09ade2562a9341
깃wooqqq <sge0225@naver.com>
2023-03-06 15:24:38 +0900

    등록할때 마다 생성되는 명언의 번호를 1씩 증가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -12,6 +12,8 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        long lastWiseSayingId = 0;
+
         while (true) {
             System.out.print("명령) ");
 
@@ -23,6 +25,16 @@ public class App {
                 case "종료":
                     System.out.println("프로그램이 종료되었습니다.");
                     return;
+                case "등록":
+                    System.out.print("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.print("작가 : ");
+                    String authorName = sc.nextLine().trim();
+
+                    long id = ++lastWiseSayingId;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
                 default:
                     System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.\n", cmd);
                     break;
